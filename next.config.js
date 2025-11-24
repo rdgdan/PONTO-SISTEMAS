72a24dd243e24b904ea5f74ec1539cd928e47009
rdgdan <danspda1@gmail.com>
2025-11-24 17:30:06 -0300

    chore(build): enable productionBrowserSourceMaps to map production errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Habilita source maps no browser em produção para facilitar o mapeamento
+  // de erros minificados (útil para debugar o React error #310).
+  productionBrowserSourceMaps: true,
 };
 
 module.exports = nextConfig;
