116cabbb8dd3b03abbd62e86086db9f83588a75e
rdgdan <danspda1@gmail.com>
2025-11-25 12:35:11 +0000

    feat: Adiciona Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import LayoutClient from "@/context/LayoutClient";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             {children}
           </LayoutClient>
         </AuthProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
